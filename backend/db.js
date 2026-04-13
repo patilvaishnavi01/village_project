af30db09910cb406439d8e50b2fdd36ab73a17a0
patilvaishnavi01 <patilvaishnavi694@gmail.com>
2026-04-13 16:00:48 +0530

    feat: initialize database connection pool and configure environment variables for frontend and backend

--- backend/db.js
+++ backend/db.js
@@ -2,18 +2,10 @@ const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
 });
-
-db.getConnection()
-    .then((connection) => {
-        console.log("✅ DB Connected");
-        connection.release();
-    })
-    .catch((err) => {
-        console.error("DB connection error:", err);
-    });
 
 module.exports = db;
