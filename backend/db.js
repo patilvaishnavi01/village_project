7ad8cb624079a1e2b2b1e0f1f6811ccc9ae57634
patilvaishnavi01 <patilvaishnavi694@gmail.com>
2026-04-13 18:44:42 +0530

    final commit errors resolved

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,14 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv"
+dotenv.config();
+console.log("---------------------------------", process.env.DB_HOST);
 
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 
     waitForConnections: true,
     connectionLimit: 10,
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import pool from "./db.js";
 
 const app = express();
-
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -22,24 +21,24 @@ app.get("/search", async (req, res) => {
         const [rows] = await pool.query(
             `
       SELECT 
-        v.village_name,
-        sd.subdistrict_name,
-        d.district_name,
-        s.state_name
-      FROM villages v
-      JOIN subdistricts sd ON v.subdistrict_id = sd.id
-      JOIN districts d ON sd.district_id = d.id
-      JOIN states s ON d.state_id = s.id
-      WHERE v.village_name LIKE ?
-      LIMIT 10
-      `,
+    v.village_name,
+    sd.subdistrict_name,
+    d.district_name,
+    s.state_name
+FROM villages v
+JOIN subdistricts sd ON v.subdistrict_code = sd.subdistrict_code
+JOIN districts d ON sd.district_code = d.district_code
+JOIN states s ON d.state_code = s.state_code
+WHERE v.village_name LIKE ?
+LIMIT 10;
+            `,
             [`%${q}%`]
         );
 
         res.json(rows);
     } catch (err) {
         console.error("DB ERROR:", err);
-        res.status(500).json({ error: "Database error" });
+        res.status(500).json({ error: err.message }); // 👈 show real error
     }
 });
 
